f6eea5d64263f2beba1cc887ffe37eb6c723a262
hunter-chambers <hrchambers.333@gmail.com>
2024-04-19 16:48:12 -0500

    abstracted away the waiting strategy

--- FalloutSoundTracks.py
+++ FalloutSoundTracks.py
@@ -23,25 +23,45 @@ SONG_URLS = []
 
 #######################################################################
 # METHODS
+def wait_until_by_xpath(element_to_search_from, condition, locator):
+    return WebDriverWait(element_to_search_from, timeout=30)\
+        .until(condition((By.XPATH, locator)))
+# end wait_until_by_xpath
+
 def parse_song_information_from_table(table_element):
-    table_header_row = WebDriverWait(table_element, timeout=30)\
-        .until(EC.visibility_of_element_located((By.XPATH, "./thead/tr")))
-    title_column_header = WebDriverWait(table_header_row, timeout=30)\
-        .until(EC.visibility_of_element_located((By.XPATH, "./th[.='Title']")))
-    song_title_column_index = WebDriverWait(table_header_row, timeout=30)\
-        .until(EC.visibility_of_all_elements_located((By.XPATH, "./th")))\
-        .index(title_column_header) + 1
+    table_header_row_locator = "./thead/tr"
+    title_col_header_locator = "./th[.='Title']"
+    table_header_elements_locator = "./th"
+
+    table_header_row = wait_until_by_xpath(table_element,
+                                           EC.visibility_of_element_located,
+                                           table_header_row_locator)
+    title_col_header = wait_until_by_xpath(table_header_row,
+                                           EC.visibility_of_element_located,
+                                           title_col_header_locator)
+    song_title_col_index = wait_until_by_xpath(table_header_row,
+                                               EC.visibility_of_all_elements_located,
+                                               table_header_elements_locator)\
+        .index(title_col_header) + 1
+
+    song_rows_locator = "./tbody/tr"
+    song_rows = wait_until_by_xpath(table_element,
+                                    EC.visibility_of_all_elements_located,
+                                    song_rows_locator)
+
+    song_title_locator = f"./td[{song_title_col_index}]"
+    song_artist_locator = f"./td[{song_title_col_index + 1}]"
 
     remove_symbols_regex = r'[^\w,()\'\.&\[\]\- ]|\[.*\]'
-    song_rows = WebDriverWait(table_element, timeout=30)\
-        .until(EC.visibility_of_all_elements_located((By.XPATH, "./tbody/tr")))
+
     for song_data in song_rows:
-        song_title = WebDriverWait(song_data, timeout=30)\
-            .until(EC.visibility_of_element_located((By.XPATH, f"./td[{song_title_column_index}]")))\
-            .get_attribute("innerText")\
-            .replace("\"", "")
-        song_artist = WebDriverWait(song_data, timeout=30)\
-            .until(EC.visibility_of_element_located((By.XPATH, f"./td[{song_title_column_index + 1}]")))\
+        song_title = wait_until_by_xpath(song_data,
+                                         EC.visibility_of_element_located,
+                                         song_title_locator)\
+            .get_attribute("innerText")
+        song_artist = wait_until_by_xpath(song_data,
+                                          EC.visibility_of_element_located,
+                                          song_artist_locator)\
             .get_attribute("innerText")
 
         song_title = re.sub(remove_symbols_regex, "", song_title)
@@ -62,17 +82,24 @@ if (__name__ == "__main__"):
     try:
         driver.get(FALLOUT_MUSIC_WIKI)
 
-        music_tables = WebDriverWait(driver, timeout=30)\
-            .until(EC.visibility_of_all_elements_located((By.XPATH, "//table[descendant::th[.='Recording date']]")))
+        music_tables_locator = "//table[descendant::th[.='Recording date']]"
+        music_tables = wait_until_by_xpath(driver,
+                                           EC.visibility_of_all_elements_located,
+                                           music_tables_locator)
+
         for table in music_tables:
             parse_song_information_from_table(table)
         # end for
 
         driver.get(YOUTUBE)
 
+        search_box_locator = "//input[@id='search']"
+
         for song_title, song_artist in SONGS.items():
-            search_box = WebDriverWait(driver, timeout=30)\
-                .until(EC.visibility_of_element_located((By.XPATH, "//input[@id='search']")))
+            search_box = wait_until_by_xpath(driver,
+                                             EC.visibility_of_element_located,
+                                             search_box_locator)
+
             search_box.clear()
             search_box.send_keys(f"{song_title} by {song_artist}")
             search_box.send_keys(Keys.RETURN)
